67c37c3073046e5f38510d4175c0fabffad6f96b
Kourosh Alasti <coding@kouroshalasti.com>
2024-10-01 21:00:47 -0700

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import updateHandler from '../controllers/update';
 
 const router = Router();
 
 router.get("/:id")
 router.post("/")
-router.put("/:id", updateHandler)
+router.put("/:id")
 router.delete("/:id")
 
 export default router;
